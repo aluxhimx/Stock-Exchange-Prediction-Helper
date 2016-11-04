37d03aabd85b72c9881ee9e0efa2af190f9daaf7
boonlai <boonlalai@gmail.com>
2016-11-04 19:54:36 +0800

    add testTen

---
+++ Project/src/testProject/testTen.java
@@ -0,0 +1,19 @@
+package testProject;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import project.Stock;
+import project.StockData;
+import project.Ten;
+
+public class testTen {
+
+	@Test
+	public void test1() {
+		Ten.calculatingEXPMA(N, m, oldEXPMA, price);
+	}	
+}
